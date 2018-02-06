2bd78bcb96197519ef91dee04b36b12cb7299af6
lishusheng <438323593@qq.com>
2018-02-06 23:36:34 +0800

    require

---
+++ src/js/config.js
@@ -0,0 +1,15 @@
+require.config({
+	paths:{
+		'jquery':'../lib/jquery-3.2.1',
+		'zoom':'../lib/jquery.gdsZoom/jquery.gdsZoom',
+		'common':'../lib/common.js',
+		'lunbo':'../lib/jquery.tree_lunbo/jquery.tree_lunbo'
+	},
+
+	shim:{
+		// 设置依赖
+		'zoom':['jquery'],
+		'common':['jquery'],
+		'lunbo':['jquery']	
+	}
+});
